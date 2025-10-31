6b58dd9bd0d52d3453bd37881e0eb4e2d260ad0f
hyebin <shin378378@naver.com>
2025-10-31 14:25:23 +0900

    Feat : 복지 카테고리 enum 만들기

--- src/main/java/greenProject/demo/enums/Category.java
+++ src/main/java/greenProject/demo/enums/Category.java
@@ -2,7 +2,21 @@ package greenProject.demo.enums;
 
 public enum Category {
 
-    HOME("자취");
+    PHYSICAL_HEALTH("신체건강"),
+    MENTAL_HEALTH("정신건강"),
+    LIVING_SUPPORT("생활지원"),
+    HOUSING("주거"),
+    JOB("일자리"),
+    CULTURE_LEISURE("문화·여가"),
+    SAFETY_CRISIS("안전·위기"),
+    PREGNANCY_CHILD_BIRTH("임신·출산"),
+    CHILDCARE("보육"),
+    EDUCATION("교육"),
+    ADOPTION_FOSTER("입양·위탁"),
+    CARE_PROTECTION("보호·돌봄"),
+    FINANCE("서민금융"),
+    LAW("법률"),
+    ENERGY("에너지");
 
     private final String type;
 
@@ -14,4 +28,3 @@ public enum Category {
         return type;
     }
 }
-
