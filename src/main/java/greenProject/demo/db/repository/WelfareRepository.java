c84005c0d83e4e74b4606c90726e1d7c202141ac
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2025-11-08 23:00:16 +0900

    Feat : welfare 레포지토리 만들기

--- src/main/java/greenProject/demo/db/repository/WelfareRepository.java
+++ src/main/java/greenProject/demo/db/repository/WelfareRepository.java
@@ -1,9 +1,16 @@
 package greenProject.demo.db.repository;
 
 import greenProject.demo.db.entity.Welfare;
+import greenProject.demo.enums.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WelfareRepository extends JpaRepository<Welfare, Long> {
+
+    List<Welfare> findByOrganizationAndStartTimeIsNotNullOrderByStartTimeAsc(Organization organization);
+    List<Welfare> findByOrganizationAndStartTimeIsNullOrderByIdDesc(Organization organization);
 }
+
