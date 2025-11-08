b03f0d17af0263f56022f534708f290bd4aec469
신혜빈 <128687822+c0mpuTurtle@users.noreply.github.com>
2025-11-08 23:00:10 +0900

    Feat : welfare 컨트롤러 만들기

--- src/main/java/greenProject/demo/api/welfare/controller/WelfareController.java
+++ src/main/java/greenProject/demo/api/welfare/controller/WelfareController.java
@@ -1,4 +1,28 @@
 package greenProject.demo.api.welfare.controller;
 
+import ch.qos.logback.core.model.Model;
+import greenProject.demo.api.welfare.service.WelfareService;
+import greenProject.demo.db.entity.Welfare;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class WelfareController {
+
+    private final WelfareService welfareService;
+
+    @GetMapping("/main")
+    public String mainPage() {
+        return "welfare-main";
+    }
+
+    @GetMapping("/savings")
+    public String savingsPage(Model model) {
+        List<Welfare> welfares = welfareService.getWelfaresByCategory(Category.SAVINGS);
+        model.addAttribute("title", "도약 계좌");
+        model.addAttribute("welfares", welfares);
+        return "support-savings"; // 템플릿 이름
+    }
 }
